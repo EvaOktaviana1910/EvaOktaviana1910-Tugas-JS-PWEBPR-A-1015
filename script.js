9f6c0d55d9329d1ea1de403142f6b44b39534727
Eva Oktaviana <146150586+EvaOktaviana1910@users.noreply.github.com>
2024-03-30 10:56:01 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+document.getElementById("Iya").addEventListener("click",function(){
+    document.getElementById("heading").innerText="Anda Berbohong!!"
+    document.getElementById("background-besar").style="background-color: #E12E2E"
+    document.getElementById("gambar-utama").src="https://s3-alpha-sig.figma.com/img/b8fd/d7c0/bcb2a8115cc9610a0c1a9d78ccb9a051?Expires=1712534400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=Sm7U4Sb2Y5lsDvy~QCdwNiisqJqaKYhREeKfDmR6IOFqFoAHvfYRvt6x1MGj3guOYiJeFgMH6UvQ-dMiAF5k-gyJsMRNImD5kusMI3nbhQv4abyYvY4ozUstxrd4l4wbj2qO~iuBxBhEG3uELrd5HbWsppKL9DSW2CUvZaFT1~XrKaA7zIJewV8Gpq3yi3um9K9yJeq7AnCZWXOkim3mQOQp1cXdcqheoeUUIDalt5o9LuktIR~t5JgCxQ6LKEQDI8d7U8efx9rPi2SCPeibee7jZp~vTHOekVyhLLm-xqNScI8V9MjiwoMs3SzV0EQkgyx5pv6uKAsXL39eJF4I0Q__"
+    document.getElementById("keterangan-flex").innerText="Anda Adalah Seorang Teknisi"
+    document.getElementById("button-pilihan").style="visibility: hidden"
+})
+
+document.getElementById("Tidak").addEventListener("click",function(){
+    document.getElementById("heading").innerText="Anda Benar!!"
+    document.getElementById("background-besar").style="background-color: #6FD240"
+    document.getElementById("gambar-utama").src="https://s3-alpha-sig.figma.com/img/f0a4/f46d/22b0da421086d344420bcc7fb45598b1?Expires=1712534400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=i7plS-dAuX5lkME1gklmfydl2tGgZ85eiCcC6LlXV9Vf4M6IkAqA7ZxYddxesJX9i~TIh8I2bBccfMuwGENv3BIc0hC0bJKcvsG2eDxXUyEu9O~WGZ-ocLIRVcdxDlxkXFE-K2ENbI2EfKby6HqYY4x7fxY9sTMil87LWBWleHzzpuDKWPWf40q6ev-cwtsnU0Q63LUQDPU9gFFwY-4DKmXSyfwfU3CHxyWL7vc7Fj~AQ1eOtzJDaN6C24qwnfRdeZYVAJNozobfZ41r-uI4vdrt5SFUjN1SazYyB-3qHIGsrgkI5300wiHaMbfWlLK6~OSnAv5iaiZhfxaH4iDL5w__"
+    document.getElementById("keterangan-flex").innerText="Anda Adalah Seorang Mahasiswa"
+    document.getElementById("button-pilihan").style="visibility: hidden"
+})
